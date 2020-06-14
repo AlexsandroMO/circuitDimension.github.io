4baddeb049493afd7ae9127a4d50e3d3ea0f422f
Alexsandro <34524511+AlexsandroMO@users.noreply.github.com>
2020-06-14 02:11:07 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,42 @@
+
+//https://respostas.guj.com.br/31984-como-criar-um-header-e-um-footer-fixos-para-todas-as-paginas
+
+
+$(document).ready(function(){
+
+    var baseUrl    = 'https://8000-a4494a87-5e96-4212-9efb-04d304f99a58.ws-us02.gitpod.io/';
+    var deleteBtn  = $('.delete-btn');
+    var searchBtn  = $('#search-btn');
+    var searchForm = $('#search-form');
+    var filtery    = $('#filtery');
+    
+
+
+    $(deleteBtn).on('click', function(e){
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer realmente deletar esta tarefa?');
+
+        if(result){
+            window.location.href = delLink;
+        }
+    });  
+
+    $(searchBtn).on('click', function(){
+        searchForm.submit();
+    })
+
+    $(filtery).change(function() {
+        var filtery = $(this).val();
+        window.location.href = baseUrl + '?filtery=' + filtery;
+        console.log(filtery);
+
+    });
+ 
+});
+
+
+function funcao_alerta(){
+    alert("Sorry, Research in Construction !!!");
+}
